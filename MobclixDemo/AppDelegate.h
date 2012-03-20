0f1286a1d1a5c82f7271bbec122358e6927cdb82
Tyler White <tylerwhitedesign@gmail.com>
2012-03-20 20:21:25 -0300

    Initial Commit

---
+++ MobclixDemo/AppDelegate.h
@@ -0,0 +1,16 @@
+//
+//  AppDelegate.h
+//  MobclixDemo
+//
+//  Created by Tyler White on 3/20/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Mobclix.h"
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
